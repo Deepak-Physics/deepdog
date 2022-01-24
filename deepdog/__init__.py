d64939f346b79798661daed477e883293b956ddd
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-01-24 11:13:24 -0600

    Don't break the build

--- deepdog/__init__.py
+++ deepdog/__init__.py
@@ -3,11 +3,12 @@ from deepdog.meta import __version__
 from deepdog.bayes_run import BayesRun
 from deepdog.diagnostic import Diagnostic
 
+
 def get_version():
 	return __version__
 
 
-__all__ = ["get_version", "BayesRun"]
+__all__ = ["get_version", "BayesRun", "Diagnostic"]
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
