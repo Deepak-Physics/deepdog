7284dbeb34ef46189d81fb719252dfa74b8e9fa8
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-03-06 16:45:09 -0600

    feat: Adds alt bayes solver with monte carlo sampler

--- deepdog/__init__.py
+++ deepdog/__init__.py
@@ -1,6 +1,7 @@
 import logging
 from deepdog.meta import __version__
 from deepdog.bayes_run import BayesRun
+from deepdog.alt_bayes_run import AltBayesRun
 from deepdog.diagnostic import Diagnostic
 
 
@@ -8,7 +9,7 @@ def get_version():
 	return __version__
 
 
-__all__ = ["get_version", "BayesRun", "Diagnostic"]
+__all__ = ["get_version", "BayesRun", "AltBayesRun", "Diagnostic"]
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
