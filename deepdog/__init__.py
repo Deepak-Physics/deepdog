492a5e6681c85f95840e28cfd5d4ce4ca1d54eba
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-04-16 12:55:29 -0500

    fix: Makes altbayessimulpairs available in package

--- deepdog/__init__.py
+++ deepdog/__init__.py
@@ -2,6 +2,7 @@ import logging
 from deepdog.meta import __version__
 from deepdog.bayes_run import BayesRun
 from deepdog.alt_bayes_run import AltBayesRun
+from deepdog.alt_bayes_run_simulpairs import AltBayesRunSimulPairs
 from deepdog.diagnostic import Diagnostic
 
 
@@ -9,7 +10,7 @@ def get_version():
 	return __version__
 
 
-__all__ = ["get_version", "BayesRun", "AltBayesRun", "Diagnostic"]
+__all__ = ["get_version", "BayesRun", "AltBayesRun", "AltBayesRunSimulPairs", "Diagnostic"]
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
