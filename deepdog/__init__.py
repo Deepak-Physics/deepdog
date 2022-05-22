d7e0f13ca55197b24cb534c80f321ee76b9c4a40
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-05-22 16:47:26 -0500

    feat: adds better import api for real data run

--- deepdog/__init__.py
+++ deepdog/__init__.py
@@ -2,6 +2,7 @@ import logging
 from deepdog.meta import __version__
 from deepdog.bayes_run import BayesRun
 from deepdog.bayes_run_simulpairs import BayesRunSimulPairs
+from deepdog.real_spectrum_run import RealSpectrumRun
 
 
 def get_version():
@@ -12,6 +13,7 @@ __all__ = [
 	"get_version",
 	"BayesRun",
 	"BayesRunSimulPairs",
+	"RealSpectrumRun",
 ]
 
 
