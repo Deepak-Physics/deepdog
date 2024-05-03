5361dada8be4950b5157862f6a92254b543889c3
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2024-05-02 22:04:49 -0500

    feat: removes legacy bayes run, technically breaking but just don't use them

--- deepdog/__init__.py
+++ deepdog/__init__.py
@@ -1,10 +1,7 @@
 import logging
 from deepdog.meta import __version__
-from deepdog.bayes_run import BayesRun
-from deepdog.bayes_run_simulpairs import BayesRunSimulPairs
 from deepdog.real_spectrum_run import RealSpectrumRun
 from deepdog.temp_aware_real_spectrum_run import TempAwareRealSpectrumRun
-from deepdog.bayes_run_with_ss import BayesRunWithSubspaceSimulation
 
 
 def get_version():
@@ -13,11 +10,8 @@ def get_version():
 
 __all__ = [
 	"get_version",
-	"BayesRun",
-	"BayesRunSimulPairs",
 	"RealSpectrumRun",
 	"TempAwareRealSpectrumRun",
-	"BayesRunWithSubspaceSimulation",
 ]
 
 
